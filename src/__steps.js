20296bf91fcfea4496643ef37b4dea779e6baab6
AkhiSharmin <akhisharmin100@gmail.com>
2023-04-15 11:47:35 +0600

    google page done

--- src/__steps.js
+++ src/__steps.js
@@ -1,4 +1,6 @@
-/**
+/**            --------------------
+ *              INITIAL SETUP
+ *            --------------------
  * 1. visit: console.firebase.google.com
  * 2. create project (skip google analytics)
  * 3. Register app (create config)
--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,27 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import app from '../../firebase/firebase.init';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 
 
 const Login = () => {
-    
+    const [user, setUser] = useState(null); // Initialize user to null
+
     const auth = getAuth(app);
-    const provider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
+
+    const githubProvider = new GithubAuthProvider();
+
+
+
 
     const handleGoogleSignIn = () => {
-       signInWithPopup(auth,provider)
-       .then(result =>{
-        const user = result;
-        console.log(user)
+       signInWithPopup(auth, googleProvider)
+       .then(result => {
+          const loggedInUser = result.user; // Get user info from result object
+          console.log(loggedInUser);
+          setUser(loggedInUser); // Set user info to user state variable
        })
        .catch(error => {
-            console.log(error)
+            console.log(error);
        })
+    }
+
+
+
+    const handelSingOut = () => {
+        signOut(auth)
+        .then(result => {
+            console.log(result)
+            setUser(null)
+        })
+        .catch(error => {
+            console.log(error)
+        })
     }
 
     return (
         <div>
-            <button onClick={handleGoogleSignIn}>Google Login</button>
+           {/* user ? logout : sing in */}
+
+            { user ?
+                <button onClick={handelSingOut}>Sing Out</button> :
+                <>
+                    <button onClick={handleGoogleSignIn}>Google Login</button>
+                    <button>Github Login</button>
+                </>
+            }
+
+            { user && <div> {/* Render user information only if user is not null */}
+                <h2>User: {user.displayName}</h2> {/* Display user's display name */}
+                <p>Email: {user.email}</p>
+                <img src={user.photoURL} alt="" />
+            </div>}
         </div>
     );
 };
